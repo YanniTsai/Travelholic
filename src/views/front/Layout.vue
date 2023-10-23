48cd8881593b8c72a96b654668a05848ad91b421
Yanni Tsai <loveyanni88@gmail.com>
2023-10-23 16:57:22 +0800

    feat: layout of customer page

---
+++ src/views/front/Layout.vue
@@ -0,0 +1,17 @@
+<template>
+  <Navbar></Navbar>
+  <router-view></router-view>
+  <Footer></Footer>
+</template>
+
+<script>
+import Navbar from '../../components/front/Front_Navbar.vue'
+import Footer from '../../components/front/Footer.vue'
+
+export default {
+  components: {
+    Navbar,
+    Footer
+  }
+}
+</script>
