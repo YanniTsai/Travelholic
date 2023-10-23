936f047a3b43f5d2fcb4194a9c212cc5ac4270fd
Yanni Tsai <loveyanni88@gmail.com>
2023-10-23 16:57:43 +0800

    feat: footer of customer page

---
+++ src/components/front/Footer.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="container-fluid bg-dark text-light footer d-flex justify-content-center align-items-center">
+        <i class="bi bi-c-circle"></i> &ensp;2023 | 僅作為練習使用，無任何商業用途。
+    </div>
+</template>
+
+<style lang="scss">
+.footer{
+    height: 80px;
+}
+</style>
