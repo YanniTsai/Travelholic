0b01e47ad32f160c00065554fa60ede396214864
Yanni Tsai <loveyanni88@gmail.com>
2023-10-24 17:41:34 +0800

    feat: pagination

---
+++ src/components/front/Front_Pagination.vue
@@ -0,0 +1,71 @@
+<template>
+  <nav aria-label="Page navigation">
+    <ul class="pagination justify-content-center">
+      <li class="page-item" :class="{ disabled: !pagination.has_pre }">
+        <a
+          class="page-link"
+          href="#"
+          aria-label="Previous"
+          @click.prevent="updatePage(1)"
+        >
+          <span aria-hidden="true">&laquo;</span>
+        </a>
+      </li>
+      <li class="page-item" :class="{ disabled: !pagination.has_pre }">
+        <a
+          class="page-link"
+          href="#"
+          aria-label="Previous"
+          @click.prevent="updatePage(pagination.current_page - 1)"
+        >
+          <span aria-hidden="true">&lsaquo;</span>
+        </a>
+      </li>
+      <li class="page-item" v-for="page in pagination.total_pages" :key="page">
+        <a
+          class="page-link"
+          href="#"
+          @click.prevent="updatePage(page)"
+          :class="{ active: page === pagination.current_page }"
+        >
+          {{ page }}
+        </a>
+      </li>
+      <li class="page-item" :class="{ disabled: !pagination.has_next }">
+        <a
+          class="page-link"
+          href="#"
+          aria-label="Next"
+          @click.prevent="updatePage(pagination.current_page + 1)"
+        >
+          <span aria-hidden="true">&rsaquo;</span>
+        </a>
+      </li>
+      <li class="page-item" :class="{ disabled: !pagination.has_next }">
+        <a
+          class="page-link"
+          href="#"
+          aria-label="Next"
+          @click.prevent="updatePage(pagination.total_pages)"
+        >
+          <span aria-hidden="true">&raquo;</span>
+        </a>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  props: {
+    pagination: {
+      type: Object
+    }
+  },
+  methods: {
+    updatePage (page) {
+      this.$emit('update-page', page)
+    }
+  }
+}
+</script>
