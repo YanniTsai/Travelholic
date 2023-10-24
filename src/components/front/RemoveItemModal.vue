6fcdec65674277e84fe9aab5fc328c253ba76654
Yanni Tsai <loveyanni88@gmail.com>
2023-10-24 17:41:18 +0800

    feat: cart-page layout and function

---
+++ src/components/front/RemoveItemModal.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="modal" tabindex="-1" ref="modal">
+    <div class="modal-dialog modal-dialog-centered">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">刪除行程</h5>
+        </div>
+        <div class="modal-body">
+          <p>確定要刪除 {{ cartItem.title }} 嗎？</p>
+        </div>
+        <div class="modal-footer">
+          <button
+            type="button"
+            class="btn btn-secondary"
+            data-bs-dismiss="modal"
+          >
+            取消
+          </button>
+          <button type="button" class="btn btn-danger" @click.prevent="$emit('remove-item')">刪除</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Modal from 'bootstrap/js/dist/modal'
+
+export default {
+  props: {
+    cartItem: {
+      type: Object
+    }
+  },
+  data () {
+    return {
+      modal: {}
+    }
+  },
+  methods: {
+    showModal () {
+      this.modal.show()
+    },
+    hideModal () {
+      this.modal.hide()
+    }
+  },
+  mounted () {
+    this.modal = new Modal(this.$refs.modal)
+  }
+}
+</script>
---
+++ src/views/front/Cart.vue
@@ -0,0 +1,200 @@
+<template>
+    <Loading :active="isLoading" style="z-index: 9999"></Loading>
+    <div class="container">
+        <h3 class="m-3">購物車</h3>
+        <div class="d-flex flex-column align-items-center" v-if="cartlength === 0">
+            <div>購物車目前沒有商品！</div>
+            <button class="btn coupon-btn">
+                <router-link to="/products" class="text-dark text-decoration-none">探索行程</router-link>
+            </button>
+        </div>
+        <div class="row" v-else>
+            <div class="col-md-7 cart-list">
+                <div class="mb-3">
+                    <div class="cart-item p-3 m-3" v-for="item in cart.carts" :key="item.id">
+                        <div class="row">
+                            <img :src="item.product.imageUrl" :alt="item.product.title" class="col-md-3">
+                            <div class="col-md-6">
+                                <router-link :to="`/products/${item.product.id}`" class="text-muted text-decoration-none">
+                                    <div class="mb-2">{{ item.product.title }}</div>
+                                    <div class="fw-light mb-2">{{ item.product.description }}</div>
+                                    <div>$ {{ item.product.price }}</div>
+                                </router-link>
+                            </div>
+                            <div class="col-md-3 d-flex align-items-center">
+                                <div class="input-group">
+                                    <input type="number" min="1" class="form-control" v-model.number="item.qty" @change="updateCart(item)">
+                                    <span class="input-group-text bg-white border-0">人</span>
+                                </div>
+                            </div>
+                        </div>
+                        <hr>
+                        <div class="d-flex justify-content-between align-items-center">
+                            <button class="btn" @click.prevent="openRemoveModal(item)">
+                                <i class="bi bi-trash3"></i>
+                            </button>
+                            <div class="fw-bold">$ {{ $filters.currency(item.total) }}</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-5">
+                <div class="mt-3 ms-3 mb-3 pt-3">
+                    <div class="mb-3">套用優惠</div>
+                    <div class="input-group mb-1">
+                        <input type="text" class="form-control" placeholder="輸入優惠碼" aria-label="coupon_code" aria-describedby="usecoupon" v-model="coupon_code">
+                        <button class="btn coupon-btn" type="button" id="usecoupon" @click.prevent="addCoupon">使用</button>
+                    </div>
+                    <div class="text-success ms-3" v-if="discont">優惠碼：{{ coupon_code }}&ensp;<i class="bi bi-check2-circle"></i></div>
+                </div>
+                <div class="d-flex justify-content-end mb-3">
+                    <div>
+                        <div class="mb-2">{{ cartlength }} 件商品 </div>
+                        <div class="d-flex align-items-center" :class="{'text-secondary': discont}">
+                            <span>總計：</span>
+                            <span class="fw-bold" :class="{'text-decoration-line-through': discont}">{{ $filters.currency(cart.total) }} 元</span>
+                        </div>
+                        <div v-if="discont">
+                            <span class="final-price">優惠價： {{ $filters.currency(cart.final_total) }} 元</span>
+                        </div>
+                        <button class="btn btn-checkout w-100 mt-3">
+                          <router-link to="/order" class="text-decoration-none text-dark">
+                            結帳去
+                            <i class="bi bi-arrow-right"></i>
+                          </router-link>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <RemoveItemModal ref="removeItemModal" :cartItem="tempItem" @remove-item="removeItem"></RemoveItemModal>
+</template>
+
+<script>
+import RemoveItemModal from '@/components/front/RemoveItemModal.vue'
+
+export default {
+  data () {
+    return {
+      cart: {},
+      cartlength: 0,
+      tempItem: {},
+      coupon_code: '',
+      discont: false,
+      isLoading: false
+    }
+  },
+  components: {
+    RemoveItemModal
+  },
+  watch: {
+    cart () {
+      this.cartlength = this.cart.carts.length
+    }
+  },
+  methods: {
+    getCart () {
+      this.isLoading = true
+      const api = `${process.env.VUE_APP_API}api/${process.env.VUE_APP_PATH}/cart`
+
+      this.$http.get(api).then((res) => {
+        this.isLoading = false
+        console.log(res.data)
+
+        this.cart = res.data.data
+      })
+    },
+    updateCart (item) {
+      this.isLoading = true
+      const cart = {
+        product_id: item.id,
+        qty: item.qty
+      }
+
+      const api = `${process.env.VUE_APP_API}api/${process.env.VUE_APP_PATH}/cart/${item.id}`
+
+      this.$http.put(api, { data: cart }).then((res) => {
+        this.isLoading = false
+        console.log(res.data)
+
+        this.getCart()
+      })
+    },
+    openRemoveModal (item) {
+      this.tempItem = item
+
+      this.$refs.removeItemModal.showModal()
+    },
+    removeItem () {
+      this.isLoading = true
+      console.log(this.tempItem)
+      const api = `${process.env.VUE_APP_API}api/${process.env.VUE_APP_PATH}/cart/${this.tempItem.id}`
+
+      this.$http.delete(api).then((res) => {
+        this.isLoading = false
+        console.log(res.data)
+
+        this.$refs.removeItemModal.hideModal()
+        this.getCart()
+      })
+    },
+    addCoupon () {
+      const coupon = {
+        code: this.coupon_code
+      }
+
+      const api = `${process.env.VUE_APP_API}api/${process.env.VUE_APP_PATH}/coupon`
+
+      this.$http.post(api, { data: coupon }).then((res) => {
+        if (res.data.success) {
+          console.log(res.data)
+          this.discont = true
+        }
+
+        this.getCart()
+      })
+    }
+  },
+  created () {
+    this.getCart()
+  }
+}
+</script>
+
+<style lang="scss">
+@import '../../assets/helpers/color';
+
+.cart-list{
+  height: 100vh;
+  overflow: auto;
+}
+.cart-item{
+  border: 1px solid #dbd5d5f5;
+  border-radius: 10px;
+
+  img{
+    max-height: 200px;
+  }
+}
+
+.coupon-btn{
+    border: 1px solid #dee2e6;
+    background: $light-yellow;
+
+    &:hover{
+      background: $main-yellow;
+    }
+}
+
+.btn-checkout{
+  background: $light-orange;
+  &:hover{
+    background: $orange;
+  }
+}
+
+.final-price{
+  color: $orange;
+}
+</style>
