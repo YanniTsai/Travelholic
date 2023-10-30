551352679584facb59aebca1f8cd1ecd08a9d624
Yanni Tsai <loveyanni88@gmail.com>
2023-10-30 13:20:12 +0800

    layout: text-nowrap

--- src/views/front/FinishOrder.vue
+++ src/views/front/FinishOrder.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="finish-page">
-        <div class="finish-content p-4 d-flex flex-column justify-content-center align-items-center">
+        <div class="finish-content text-nowrap p-4 d-flex flex-column justify-content-center align-items-center">
             <h3>Thank you!</h3>
             <div>謝謝您的訂購。</div>
             <div>若有任何問題，歡迎隨時與我們聯繫。</div>
