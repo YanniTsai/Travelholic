ee3dbc6f61e0ff034938bb7d6990ca5c4f63c6df
Yanni Tsai <loveyanni88@gmail.com>
2023-11-06 16:46:53 +0800

    feat: add router-link to client page

--- src/components/admin/Navbar.vue
+++ src/components/admin/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">後台管理</a>
+      <router-link class="navbar-brand" to="/dashboard/products">後台管理</router-link>
       <button
         class="navbar-toggler"
         type="button"
@@ -19,6 +19,7 @@
           <router-link to="/dashboard/orders" class="nav-link" :class="{'fw-bold': $route.path === '/dashboard/orders'}">訂單管理</router-link>
           <router-link to="/dashboard/coupons" class="nav-link" :class="{'fw-bold': $route.path === '/dashboard/coupons'}">優惠券管理</router-link>
           <router-link to="/dashboard/articles" class="nav-link" :class="{'fw-bold': $route.path === '/dashboard/articles'}">文章管理</router-link>
+          <router-link to="/home" class="nav-link">前台網頁</router-link>
         </ul>
         <a href="#"  @click.prevent="logout" class="nav-link">登出</a>
       </div>
